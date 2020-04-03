cecd4db83060eb0f1574d3e5f88424e5fa5efbd5
Mike Sisk <mike.sisk@tbs-sct.gc.ca>
2020-04-03 17:28:20 +0000

    Finishing the publish script

--- pipeline/prep_and_publish_data.py
+++ pipeline/prep_and_publish_data.py
@@ -6,12 +6,14 @@ def run_prep_and_publish(csv_file, endpoint, api_key, dataset_id, resource_id, d
     print('>>Publishing {0:s}'.format(dataset_title))
 
     ua = 'prep_and_publish_data.py/1.0 (+{0:s})'.format(endpoint)
-
     reg = RemoteCKAN(endpoint, apikey=api_key, user_agent=ua)
-
     resource_name = os.path.basename(csv_file)
-    with open(csv_file, encoding='utf-8-sig') as f:
-        pkg = reg.action.resource_patch(
-            id=resource_id,
-            upload=(resource_name, f)
-        )
+    
+    try:
+        with open(csv_file, encoding='utf-8-sig') as f:
+            pkg = reg.action.resource_patch(
+                id=resource_id,
+                upload=(resource_name, f)
+            )
+    except:
+        print('\tERROR: Publish failed.')
