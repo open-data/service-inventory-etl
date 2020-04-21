fc473123195a8efe721b212ce18fbdb94678c639
Mike Sisk <mike.sisk@tbs-sct.gc.ca>
2020-04-21 13:47:49 +0000

    Adjusting open function to try and force UTF-8 with BOM after publishing

--- pipeline/prep_and_publish_data.py
+++ pipeline/prep_and_publish_data.py
@@ -1,6 +1,6 @@
 import pandas as pd
 from ckanapi import RemoteCKAN, NotAuthorized
-import os, glob
+import os, glob, io
 
 def run_prep_and_publish(csv_file, endpoint, api_key, dataset_id, resource_id, dataset_title):
     print('>>Publishing {0:s}'.format(dataset_title))
@@ -10,10 +10,10 @@ def run_prep_and_publish(csv_file, endpoint, api_key, dataset_id, resource_id, d
     resource_name = os.path.basename(csv_file)
     
     try:
-        with open(csv_file, encoding='utf-8-sig') as f:
+        with io.open(csv_file, encoding='utf-8-sig') as f:
             pkg = reg.action.resource_patch(
                 id=resource_id,
-                upload=(resource_name, f)
+                upload=(resource_name, f.read())
             )
     except:
         print('\tERROR: Publish failed.')
